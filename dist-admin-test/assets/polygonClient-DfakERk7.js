5074cf2025a6d7a61f23fac0beb9131563a287f5
TONY <phoenixapril@yahoo.com>
2026-04-12 18:36:44 -0700

    cleanup: remove ~2800 lines of dead code

---
+++ dist-admin-test/assets/polygonClient-DfakERk7.js
@@ -0,0 +1,1 @@
+import{s as a}from"./index-Bn_uDxCj.js";async function s(r){const{data:t,error:o}=await a.functions.invoke("polygon-proxy",{body:{path:r}});if(o)throw o;return t}export{s as p};
--- src/components/tabs/homeConstants.js
+++
@@ -1,7 +0,0 @@
-export const FUTURES_MAP = {
-  'ES=F': 'S&P Fut',
-  'NQ=F': 'Nas Fut',
-  'YM=F': 'Dow Fut',
-  'GC=F': 'Gold',
-  'CL=F': 'Oil',
-};
